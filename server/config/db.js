e119f83777b564640e076314028c98cd222ba04f
anth0nywong <anthonywong.hokhei@gmail.com>
2022-08-01 06:48:32 -0400

    remote connection

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ exports.LocalURI = "mongodb://127.0.0.1/survey";
 exports.RemoteURI = "mongodb+srv://anthony:GAAHRYJU01gGusEX@cluster0.ruhrxoy.mongodb.net/?retryWrites=true&w=majority";
 exports.HostName = "MongoDB Atlas";
 exports.Secret = "someSecret";
-exports.user = "HEI";
-exports.password = "ab92WWak";
-exports.connectString = "localhost:1521/xe";
+exports.user = "admin";
+exports.password = "abc920WWak#!$^";
+exports.connectString = "db20220722165842_low";
 //# sourceMappingURL=db.js.map
--- server/config/db.ts
+++ server/config/db.ts
@@ -2,9 +2,9 @@ export const LocalURI = "mongodb://127.0.0.1/survey";
 export const RemoteURI = "mongodb+srv://anthony:GAAHRYJU01gGusEX@cluster0.ruhrxoy.mongodb.net/?retryWrites=true&w=majority"
 export const HostName = "MongoDB Atlas";
 export const Secret = "someSecret";
-// export const user = "admin";
-// export const password = "abc920WWak#!$^";
-// export const connectString = "db20220722165842_low";
-export const user = "HEI";
-export const password = "ab92WWak";
-export const connectString = "localhost:1521/xe";
+export const user = "admin";
+export const password = "abc920WWak#!$^";
+export const connectString = "db20220722165842_low";
+// export const user = "HEI";
+// export const password = "ab92WWak";
+// export const connectString = "localhost:1521/xe";
