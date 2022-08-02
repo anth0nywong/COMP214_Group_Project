58d31f1da234003f0f2fb6d88841d98aa3247a37
anth0nywong <anthonywong.hokhei@gmail.com>
2022-08-02 18:05:59 -0400

    order complete page

--- server/controllers/flights.ts
+++ server/controllers/flights.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as DBConfig from '../config/db';
 import oracledb from 'oracledb';
 
+//oracledb.initOracleClient({ libDir: 'C:/oracle/instantclient_21_6' }); <-- uncomment this line if using window
 let userId = 10;
 
 export function DisplayFlightsPage (req: express.Request, res: express.Response, next: express.NextFunction)
