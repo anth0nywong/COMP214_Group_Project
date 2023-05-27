3ada13f818a7d7e0e8c6d8c05675314f32c809ca
anth0nywong <87212104+anth0nywong@users.noreply.github.com>
2023-05-26 21:56:56 -0400

    Delete confidential information

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectString = exports.password = exports.user = exports.Secret = void 0;
-exports.Secret = "someSecret";
+exports.Secret = "";
 exports.user = "admin";
 exports.password = "";
 exports.connectString = "db20220722165842_low";
